7b4120b48f8e9240e0b4b1f9b242b787c3a233e7
Ashutosh Agrawal <webashutosh@gmail.com>
2019-08-02 09:46:55 +0530

    Initial Commit

---
+++ src/main/java/in/acode/App.java
@@ -0,0 +1,13 @@
+package in.acode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
