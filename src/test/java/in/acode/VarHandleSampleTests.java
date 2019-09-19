1a410360ca8c850faef27ae69bf910ef047fb838
Ashutosh Agrawal <webashutosh@gmail.com>
2019-09-19 22:37:48 +0530

    Var Handle Tests 1

---
+++ src/test/java/in/acode/VarHandleSampleTests.java
@@ -0,0 +1,57 @@
+package in.acode;
+
+import org.junit.Test;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class VarHandleSampleTests {
+
+    //Class to hold a variable and a corresponding var-handle
+    static class VarHandleContainer {
+        public int x;
+        public static final VarHandle X;
+
+        static {
+            try {
+                X = MethodHandles.lookup().findVarHandle(VarHandleContainer.class, "x", int.class);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Test(timeout = 1000)
+    public void shouldOpaqueModeDetectInterthreadChangesToSameVar() throws NoSuchFieldException, IllegalAccessException, InterruptedException {
+
+        VarHandleContainer varHandleContainer = new VarHandleContainer();
+        varHandleContainer.x = 0;
+
+        //Set x to 1 after sometime
+        executeAfterDelay(() -> VarHandleContainer.X.setOpaque(varHandleContainer, 1),
+                500, TimeUnit.MILLISECONDS);
+
+
+        //Start a thread which would exit only after the changed value of x is visible
+        Thread thread1 = new Thread(() -> {
+            while (true) {
+                if ((int) VarHandleContainer.X.getOpaque(varHandleContainer) == 1) {
+                    System.out.println("Successfully read the value written by another thread in opaque mode!");
+                    break;
+                }
+            }
+        });
+
+        thread1.start();
+        thread1.join();
+    }
+
+    private void executeAfterDelay(Runnable r, long delay, TimeUnit timeUnit) {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.schedule(r, delay, timeUnit);
+    }
+
+}
