67b4016761650e58fb9a89f6fc8c21c9b225317e
Ashutosh Agrawal <webashutosh@gmail.com>
2019-09-19 22:46:15 +0530

    Var Handle Tests 2

--- src/test/java/in/acode/VarHandleSampleTests.java
+++ src/test/java/in/acode/VarHandleSampleTests.java
@@ -8,11 +8,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.assertEquals;
+
 public class VarHandleSampleTests {
 
     //Class to hold a variable and a corresponding var-handle
     static class VarHandleContainer {
         public int x;
+        public int y;
         public static final VarHandle X;
 
         static {
@@ -26,9 +29,7 @@ public class VarHandleSampleTests {
 
     @Test(timeout = 1000)
     public void shouldOpaqueModeDetectInterthreadChangesToSameVar() throws NoSuchFieldException, IllegalAccessException, InterruptedException {
-
         VarHandleContainer varHandleContainer = new VarHandleContainer();
-        varHandleContainer.x = 0;
 
         //Set x to 1 after sometime
         executeAfterDelay(() -> VarHandleContainer.X.setOpaque(varHandleContainer, 1),
@@ -40,6 +41,33 @@ public class VarHandleSampleTests {
             while (true) {
                 if ((int) VarHandleContainer.X.getOpaque(varHandleContainer) == 1) {
                     System.out.println("Successfully read the value written by another thread in opaque mode!");
+                    break;
+                }
+            }
+        });
+
+        thread1.start();
+        thread1.join();
+    }
+
+    @Test(timeout = 1000)
+    public void shouldRAModeDetectInterthreadChangesToAllVars() throws NoSuchFieldException, IllegalAccessException, InterruptedException {
+        VarHandleContainer varHandleContainer = new VarHandleContainer();
+
+        //Set variable values after some time
+        executeAfterDelay(() -> {
+                    varHandleContainer.y = 1;
+                    VarHandleContainer.X.setRelease(varHandleContainer, 1);
+                },
+                500, TimeUnit.MILLISECONDS);
+
+
+        //Start a thread which would exit only after the changed value of x is visible
+        Thread thread1 = new Thread(() -> {
+            while (true) {
+                if ((int) VarHandleContainer.X.getAcquire(varHandleContainer) == 1) {
+                    assertEquals(1, varHandleContainer.y);
+                    System.out.println("Successfully read the values written by another thread in RA mode!");
                     break;
                 }
             }
